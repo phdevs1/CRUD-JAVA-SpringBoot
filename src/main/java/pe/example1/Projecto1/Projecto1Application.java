b9b302c5a805c545b8947e33d1c14d99fa0606ca
ph <pio.huisa@gmail.com>
2025-07-23 10:18:50 -0500

    start project

---
+++ src/main/java/pe/example1/Projecto1/Projecto1Application.java
@@ -0,0 +1,13 @@
+package pe.example1.Projecto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projecto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/example1/Projecto1/Projecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.example1.Projecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projecto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
