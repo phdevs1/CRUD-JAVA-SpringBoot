a497a99b05b514b9f38ed5bfdd54a9ca9da798c6
ph <pio.huisa@gmail.com>
2025-08-14 00:09:03 -0500

    feat: add swagger and list client

--- src/main/java/pe/example1/Projecto1/service/IClientService.java
+++ src/main/java/pe/example1/Projecto1/service/IClientService.java
@@ -3,7 +3,12 @@ package pe.example1.Projecto1.service;
 import pe.example1.Projecto1.model.dto.ClientDto;
 import pe.example1.Projecto1.model.entity.Client;
 
+import java.util.List;
+
 public interface IClientService {
+
+    List<Client> list();
+
     Client save(ClientDto client);
 
     Client findById(Integer id);
